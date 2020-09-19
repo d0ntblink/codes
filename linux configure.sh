5967e3278af1dad191257d4234225bf096d20fea
Garshasb Khodayari <33300872+GarshasbK@users.noreply.github.com>
2020-09-19 13:12:53 -0700

    Create linux configure.sh

---
+++ linux configure.sh
@@ -0,0 +1,8 @@
+#aliaes
+alias ll='ls -al'
+alias 'partytime'='/home/d0ntblink/Desktop/fortnite_dance.sh'
+alias install='apt-get install'
+alias aptg='apt-get'
+
+clear
+figlet -f swampland "d0nt blink" | lolcat -a -d 1 -p 1 -F 0.2
