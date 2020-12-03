96a73f829c169a7cf7d21047a61accbcbf9b39be
D0ntblink <33300872+d0ntblink@users.noreply.github.com>
2020-12-02 17:38:22 -0800

    Update linux configure.sh

--- Bash/linux configure.sh
+++ Bash/linux configure.sh
@@ -1,8 +1,17 @@
 #aliaes
+alias ls='ls --color=always -rthla'
 alias ll='ls -al'
-alias 'partytime'='/home/d0ntblink/Desktop/fortnite_dance.sh'
 alias install='apt-get install'
 alias aptg='apt-get'
+
+function apt-updater {
+	apt-get update &&
+	apt-get dist-upgrade -Vy &&
+	apt-get autoremove -y &&
+	apt-get autoclean &&
+	apt-get clean &&
+	reboot
+	}
 
 clear
 figlet -f swampland "d0nt blink" | lolcat -a -d 1 -p 1 -F 0.2
