15fdc8c8d6685570308f16c59d8da4360c051633
D0ntblink <33300872+d0ntblink@users.noreply.github.com>
2021-01-04 16:50:25 -0800

    Update linux configure.sh

--- Bash/linux configure.sh
+++ Bash/linux configure.sh
@@ -15,3 +15,7 @@ function apt-updater {
 
 clear
 figlet -f swampland "d0nt blink" | lolcat -a -d 1 -p 1 -F 0.2
+
+xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync
+xrandr --addmode Virtual1 1920x1080_60.00
+xrandr -s 1920x1080_60.00
